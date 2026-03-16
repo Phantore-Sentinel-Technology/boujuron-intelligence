a50343b10947001b588d5e33c7157bfcdd5319a7
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-16 18:00:57 +0100

    Fix Kafka consumer and Neon DB connection by ENGR. IPAYE

--- infastructures/kafka-consumer/consumer.py
+++ infastructures/kafka-consumer/consumer.py
@@ -1,33 +1,24 @@
 from confluent_kafka import Consumer
 import json
 import psycopg2
+from config.settings import settings
 
 conf = {
-    'bootstrap.servers': 'BOOTSTRAP_SERVER',
-    'security.protocol': 'SASL_SSL',
-    'sasl.mechanisms': 'PLAIN',
-    'sasl.username': 'API_KEY',
-    'sasl.password': 'API_SECRET',
-    'group.id': 'phantore-group',
-    'auto.offset.reset': 'earliest'
+    "bootstrap.servers": settings.KAFKA_BOOTSTRAP_SERVER,
+    "group.id": "phantore-group",
+    "auto.offset.reset": "earliest"
 }
 
 consumer = Consumer(conf)
+consumer.subscribe([settings.KAFKA_TOPIC_EVENTS])
 
-consumer.subscribe(["user_events"])
-
-conn = psycopg2.connect(
-    "postgresql://USER:PASSWORD@HOST:5432/postgres"
-)
-
+conn = psycopg2.connect(settings.DATABASE_URL)
 cursor = conn.cursor()
 
 print("Consumer started...")
 
 while True:
-
     msg = consumer.poll(1.0)
-
     if msg is None:
         continue
 
@@ -38,7 +29,6 @@ while True:
         INSERT INTO events (user_id,event_type,device,ip,timestamp)
         VALUES (%s,%s,%s,%s,%s)
         """,
-
         (
             event["user_id"],
             event["event_type"],
@@ -49,5 +39,4 @@ while True:
     )
 
     conn.commit()
-
     print("Stored event:", event)
