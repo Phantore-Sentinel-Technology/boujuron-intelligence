66945bb22ff0878d844806211dd097370eb752da
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-16 18:12:34 +0100

    Fix config

--- services/event-ingestion-service/kafka_producer.py
+++ services/event-ingestion-service/kafka_producer.py
@@ -1,7 +1,7 @@
 from confluent_kafka import Producer
 import json
 
-from config import settings
+from config.settings import settings
 
 conf = {
     "bootstrap.servers": settings.KAFKA_BOOTSTRAP_SERVER
