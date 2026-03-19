15008fcc67403665ab9bda87f70a00d087fa27f9
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-19 17:59:23 +0100

    updated consumer service updated by ENGR. IPAYE

--- infrastructure/kafka_consumer/consumer.py
+++ infrastructure/kafka_consumer/consumer.py
@@ -1,8 +1,9 @@
 from config.settings import settings
 from kafka import KafkaConsumer
-import psycopg2
+import sqlite3
 import json
 
+# ✅ Kafka consumer
 consumer = KafkaConsumer(
     settings.KAFKA_TOPIC_EVENTS,
     bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVER,
@@ -11,23 +12,38 @@ consumer = KafkaConsumer(
     group_id="phantore-group"
 )
 
-conn = psycopg2.connect(settings.DATABASE_URL)
+# ✅ SQLite DB
+conn = sqlite3.connect("events.db", check_same_thread=False)
 cursor = conn.cursor()
+
+# ✅ Create table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    event_type TEXT,
+    device TEXT,
+    ip TEXT,
+    timestamp TEXT
+)
+""")
+conn.commit()
 
 print("Consumer started...")
 
+# ✅ Process events
 for message in consumer:
     event = message.value
 
     cursor.execute(
         """
         INSERT INTO events (user_id,event_type,device,ip,timestamp)
-        VALUES (%s,%s,%s,%s,%s)
+        VALUES (?,?,?,?,?)
         """,
         (
             event["user_id"],
             event["event_type"],
-            event["device"],
+            event["device_type"],  # ⚠️ IMPORTANT FIX
             event["ip"],
             event["timestamp"]
         )
