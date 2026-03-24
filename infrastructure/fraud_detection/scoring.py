05893cd5cc0f2f29d7451390b20576b8f3bbf213
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-03-24 13:07:10 +0100

    feat: updated scoring services by ENGR. IPAYE

---
+++ infrastructure/fraud_detection/scoring.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+def calculate_risk_score(event, reason):
+    score = 0
+
+    # Base scoring
+    if reason == "too many requests":
+        score += 40
+    elif reason == "multiple Ips detected":
+        score += 30
+    elif reason == "multiple device detected":
+        score += 30
+
+    # Time base risk
+    hour = datetime.fromisoformat(event["timestamp"]).hour
+    if hour < 6 or hour > 22:
+        score += 20  # Suspicious hours
+
+    # Device risk
+    if event["device_type"].lower() == "unknown":
+        score += 25
+
+    # Normalize to 100
+    return min(score, 100)
