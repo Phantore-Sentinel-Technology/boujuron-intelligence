c5c0a88918635f61570798bed07719ebdc29966d
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-04-13 10:22:16 +0100

    feat: updated ML AI service model by ENGR. IPAYE

--- infrastructure/ml/train_model.py
+++ infrastructure/ml/train_model.py
@@ -1,26 +1,22 @@
-import sqlite3
+import psycopg2
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 import joblib
 
-# Connect DB
-conn = sqlite3.connect("events.db")
+from config.settings import settings
 
-# Load features
+# Connect Postgres
+conn = psycopg2.connect(settings.DATABASE_URL)
+
+# Load data
 df = pd.read_sql_query("SELECT * FROM ml_features", conn)
 
 if df.empty:
     print("❌ No data to train model")
     exit()
 
-# Select features
-X = df[[
-    "num_devices",
-    "num_ips",
-    "total_requests"
-]]
+X = df[["num_devices", "num_ips", "total_requests"]]
 
-# Train model
 model = IsolationForest(
     n_estimators=100,
     contamination=0.05,
@@ -29,7 +25,6 @@ model = IsolationForest(
 
 model.fit(X)
 
-# Save model
 joblib.dump(model, "fraud_model.pkl")
 
 print("✅ Model trained and saved")
