8d2a90d3d7d9d976194006ed319353005cb960fb
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-04-13 10:05:13 +0100

    feat: updated fraud model by ENGR. IPAYE

--- infrastructure/fraud_alert_service/alert_consumer.py
+++ infrastructure/fraud_alert_service/alert_consumer.py
@@ -8,5 +8,24 @@ consumer = None
 
 while consumer is None:
     try:
-        print("Connecting to fraud topic...")
-        consumer =
+        print("Connecting to Fraud Topic...")
+        consumer = KafkaConsumer(
+            "fraud_alerts",
+            bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVER,
+            value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+            auto_offset_reset="earliest",
+            group_id="fraud-alert-group"
+        )
+        print("Connected to fraud_alerts ✅")
+    except:
+        print("Retrying Kafka...")
+        time.sleep(5)
+
+print("🚨 Fraud Alert Service Started...")
+
+for message in consumer:
+    alert = message.value
+
+    print("⚠️ ALERT RECEIVED:", alert)
+
+    send_alert(alert)
