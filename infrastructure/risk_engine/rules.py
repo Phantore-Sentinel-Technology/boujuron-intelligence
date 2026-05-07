fa14007429adbdbd65a724b7df366b236306d5c2
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2026-05-07 14:12:39 +0100

    feat: updated infrastructure service by ENGR. IPAYE

--- infrastructure/risk_engine/rules.py
+++ infrastructure/risk_engine/rules.py
@@ -5,20 +5,34 @@ def check_rules(event, profile):
     reasons = []
     score = 0
 
-    # New device
-    if event["device_type"] not in profile["devices"]:
-        reasons.append("New device")
-        score += 20
+    # =========================
+    # NEW DEVICE
+    # =========================
+    if event["device_type"] not in profile["known_devices"]:
+        score += 25
+        reasons.append("New device detected")
 
-    # New IP
-    if event["ip"] not in profile["ips"]:
-        reasons.append("New Ip address")
-        score += 20
+    # =========================
+    # NEW IP
+    # =========================
+    if event["ip"] not in profile["known_ips"]:
+        score += 25
+        reasons.append("New IP detected")
 
-    # Old login time (Night activity)
+    # =========================
+    # TOO MANY REQUESTS
+    # =========================
+    if profile["total_requests"] > 10:
+        score += 30
+        reasons.append("High request frequency")
+
+    # =========================
+    # SUSPICIOUS LOGIN TIME
+    # =========================
     hour = datetime.fromisoformat(event["timestamp"]).hour
-    if hour < 5 or hour > 23:
-        reasons.append("Unusual login time")
-        score += 15
+
+    if hour < 5:
+        score += 20
+        reasons.append("Suspicious login time")
 
     return score, reasons
