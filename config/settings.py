e8f1e6544423237aadbce71374283149513c52d8
mac <@b.tunde.ipaye@gmail.com>
2026-05-27 04:54:45 +0100

    feat: improved service integration by ENGR. IPAYE

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,16 @@
-from pydantic_settings import BaseSettings
 from functools import lru_cache
 from typing import Optional
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True,
+        extra="ignore"
+    )
+
     # Application
     APP_NAME: str = "Boujuron"
     ENVIRONMENT: str = "development"
@@ -13,19 +20,13 @@ class Settings(BaseSettings):
     KAFKA_BOOTSTRAP_SERVER: str
     KAFKA_TOPIC_EVENTS: str = "user_events"
 
-    # ✅ MAKE OPTIONAL
     KAFKA_API_KEY: Optional[str] = None
     KAFKA_API_SECRET: Optional[str] = None
 
     # Database
     DATABASE_URL: str
 
-    # ✅ MAKE OPTIONAL
     API_KEY: Optional[str] = None
-
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
 
 
 @lru_cache()
